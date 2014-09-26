39aa77724e3f4e77fd9a8e3c036778ddc130a8f8
Dong Mai <dmai@colgate.edu>
2014-09-26 13:35:01 -0400

    Update list.h

--- list.h
+++ list.h
@@ -2,9 +2,17 @@
 #define __LIST_H__
 
 /* your list data structure declarations */
-
+struct node {
+    int value;
+    struct node *next; 
+};
 
 /* your function declarations associated with the list */
-
+void list_clear(struct node *);
+void list_print(const struct node *);
+int list_delete(int , struct node **);
+void list_append(int , struct node **);
+void list_reverse(struct node **);
+void list_sort(struct node **);
 
 #endif // __LIST_H__
